68901e6f7e6e183429e7c516f05447ee1e72625b
Anastasiya Gurianova <gurianova.nast@gmail.com>
2025-06-01 06:04:03 +0300

    fix: resolve TypeScript module resolution errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'@components': 'src/components',
-			'@page': 'src/page',
-			'@utils': 'src/utils',
+			components: path.resolve(__dirname, 'src/components'),
+			pages: path.resolve(__dirname, 'src/pages'),
+			utils: path.resolve(__dirname, 'src/utils'),
+			types: path.resolve(__dirname, 'src/types'),
 		},
 	},
 });
